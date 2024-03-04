7de4aa8009392d2db37cf4c0bd18f920d1232d3f
Kumar Akshat <kakshat247@gmail.com>
2024-03-04 21:46:32 +0530

    creating the home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <>
+        <section>
+            
+            {/* Heading & Button */}
+            <div>
+                <div>
+                    <h1 className='text-white'>Employees</h1>
+                    <p className='text-white'>This is the list of all the employees. You can add new employees, delete or even update the details of the employees</p>
+                </div>
+
+                <div>
+                    <button>Add Employee</button>
+                </div>
+            </div>
+
+            {/* Employee Details Table */}
+            <div>
+
+            </div>
+
+        </section>
+    </>
   )
 }
 

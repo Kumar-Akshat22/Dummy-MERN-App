e8e6368ec7dfbc8dee3737f711d0b356bbc261e5
Kumar Akshat <kakshat247@gmail.com>
2024-03-06 22:24:56 +0530

    designing the form page

--- frontend/src/pages/CreateEmployeePage.js
+++ frontend/src/pages/CreateEmployeePage.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import EmployeeForm from '../components/EmployeeForm'
+import FormImage from "../assets/Form Page.jpg"
 
 function CreateEmployeePage() {
   return (
-    <div>CreateEmployeePage</div>
+    <section>
+      <div className=''>
+        <div className='relative'> 
+          <div>
+            <img src={FormImage} width="1260" height="750"></img>
+          </div>
+
+          {/* Gradient */}
+          <div></div>
+
+          {/* Text */}
+          <div className='w-full max-w-lg absolute bottom-6 left-[20%]'>
+            <p className='text-white text-4xl font-bold'>Empower your business with our employee creation!</p>
+          </div>
+
+        </div>
+        <div>
+
+          <div>
+            <h1>Create a Employee</h1>
+            <span>Back to all Employee List</span>
+          </div>
+
+          <EmployeeForm />
+        </div>
+
+
+      </div>
+    </section>
   )
 }
 
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
 
@@ -38,40 +39,50 @@ function HomePage() {
                     <p className='text-white'>This is the list of all the employees. You can add new employees, delete or even update the details of the employees</p>
                 </div>
 
-                <div>
-                    <button className='px-3.5 py-1.5 bg-indigo-600 text-white text-sm rounded-md font-bold hover:bg-indigo-500 leading-7'>Add Employee</button>
-                </div>
+                <Link to={"/addemployee"}>
+                    <div>
+                        <button className='px-3.5 py-1.5 bg-indigo-600 text-white text-sm rounded-md font-bold hover:bg-indigo-500 leading-7'>Add Employee</button>
+                    </div>
+                </Link>
             </div>
 
             {/* Employee Details Table */}
-            <div className=''>
-                <table className='w-full divide-y divide-gray-200'>
+            <div className='mt-6 border border-gray-200 dark:border-gray-700 rounded-md'>
+                <table className='w-full divide-y divide-gray-700'>
                     <thead className='bg-gray-50 dark:bg-gray-800'>
                         <tr>
-                            <th>Employee</th>
-                            <th>Title</th>
-                            <th>Role</th>
+                            <th className='text-left py-3.5 px-4 text-sm text-gray-400 font-normal'>
+                                Employee
+                            </th>
+
+                            <th className='text-left py-3.5 text-sm text-gray-400 font-normal'>
+                                Title
+                            </th>
+
+                            <th className='text-left py-3.5 text-sm text-gray-400 font-normal'>
+                                Role
+                            </th>
                         </tr>
                     </thead>
 
-                    <tbody className=''>
+                    <tbody className='bg-gray-900 divide-y divide-gray-700'>
                         {
                             empData.map((person)=>(
-                                <tr key={person.name}>
+                                <tr key={person._id} className='text-white'>
                                     {/* Image, Name & Email */}
-                                    <td>
-                                        <div>
+                                    <td className='px-[1rem] py-4'>
+                                        <div className='flex'>
                                             <div className='w-10 h-10'>
                                                 <img className="w-full h-full rounded-full object-cover" src={person.image}></img>
                                             </div>
 
-                                            <div>
+                                            <div className='ml-4'>
                                                 <div>
-                                                    <h2>{person.name}</h2>
+                                                    <h2 className='text-sm'>{person.name}</h2>
                                                 </div>
 
                                                 <div>
-                                                    <span>{person.email}</span>
+                                                    <span className='text-sm'>{person.email}</span>
                                                 </div>
                                             </div>
                                         </div>
@@ -80,16 +91,16 @@ function HomePage() {
                                     <td>
                                         <div>
                                             <div>
-                                                <p>{person.title}</p>
+                                                <p className='text-sm'>{person.title}</p>
                                             </div>
                                             <div>
-                                                <p>{person.dpartment}</p>
+                                                <p className='text-sm'>{person.department}</p>
                                             </div>
                                         </div>
                                     </td>
                                     {/* Role */}
                                     <td>
-                                        <div>
+                                        <div className='text-sm'>
                                             {person.role}
                                         </div>
                                     </td>

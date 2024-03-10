42e9bbf340c4aef0634e700355ea6f56715ef626
Kumar Akshat <kakshat247@gmail.com>
2024-03-10 23:33:41 +0530

    styling the form component

--- frontend/src/components/EmployeeForm.js
+++ frontend/src/components/EmployeeForm.js
@@ -43,49 +43,49 @@ function EmployeeForm() {
     <div>
 
       <form onSubmit={submitHandler}>
-        <div>
+        <div className='mt-4 space-y-5'>
           <div>
-            <label htmlFor='name'>Employee Name</label>
+            <label htmlFor='name' className='text-gray-200 font-semibold text-base'>Employee Name</label>
         
             <div>
-              <input type='text' placeholder='Enter Your Full Name' name='name' onChange={changeHandler} value={formData.name}></input>
+              <input type='text' placeholder='Enter Your Full Name' name='name' onChange={changeHandler} value={formData.name} className='w-full rounded-md h-10 py-2 px-3 bg-transparent text-sm border border-gray-700 mt-2 placeholder:text-gray-400 text-gray-50 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-gray-400 focus:ring-offset-gray-900'></input>
             </div>
           </div>
 
           <div>
-            <label htmlFor='email'>Employee Email Id</label>
+            <label htmlFor='email' className='text-gray-200 font-semibold text-base'>Employee Email Id</label>
 
             <div>
-              <input type='email' placeholder='Enter Your Email' name='email' onChange={changeHandler} value={formData.email}></input>
+              <input type='email' placeholder='Enter Your Email' name='email' onChange={changeHandler} value={formData.email} className='w-full rounded-md h-10 py-2 px-3 bg-transparent text-sm border border-gray-700 mt-2 placeholder:text-gray-400 text-gray-50 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-gray-400 focus:ring-offset-gray-900'></input>
             </div>
           </div>
 
           <div>
-            <label htmlFor='title'>Employee Title</label>
+            <label htmlFor='title' className='text-gray-200 font-semibold text-base'>Employee Title</label>
 
             <div>
-              <input type='text' placeholder='Enter Your Employee Title' name='title' onChange={changeHandler} value={formData.title}></input>
+              <input type='text' placeholder='Enter Your Employee Title' name='title' onChange={changeHandler} value={formData.title} className='w-full rounded-md h-10 py-2 px-3 bg-transparent text-sm border border-gray-700 mt-2 placeholder:text-gray-400 text-gray-50 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-gray-400 focus:ring-offset-gray-900'></input>
             </div>
           </div>
 
           <div>
-            <label htmlFor='department'>Employee Department</label>
+            <label htmlFor='department' className='text-gray-200 font-semibold text-base'>Employee Department</label>
 
             <div>
-              <input type='text' placeholder='Enter Your Employee Department' name='department' onChange={changeHandler} value={formData.department}></input>
+              <input type='text' placeholder='Enter Your Employee Department' name='department' onChange={changeHandler} value={formData.department} className='w-full rounded-md h-10 py-2 px-3 bg-transparent text-sm border border-gray-700 mt-2 placeholder:text-gray-400 text-gray-50 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-gray-400 focus:ring-offset-gray-900'></input>
             </div>
           </div>
 
           <div>
-            <label htmlFor='role'>Employee Role</label>
+            <label htmlFor='role' className='text-gray-200 font-semibold text-base'>Employee Role</label>
 
             <div>
-              <input type='text' placeholder='Enter Your Employee Role' name='role' onChange={changeHandler} value={formData.role}></input>
+              <input type='text' placeholder='Enter Your Employee Role' name='role' onChange={changeHandler} value={formData.role} className='w-full rounded-md h-10 py-2 px-3 bg-transparent text-sm border border-gray-700 mt-2 placeholder:text-gray-400 text-gray-50 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-gray-400 focus:ring-offset-gray-900'></input>
             </div>
           </div>
 
           <div>
-            <button className='flex items-center gap-1 text-white' type='submit'>
+            <button className='w-full flex justify-center gap-1 text-white' type='submit'>
               Create Employee
               <FaAngleRight />
             </button>
